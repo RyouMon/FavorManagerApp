7b151278e6a85e8e4dacf540e46c2c32a3a6337c
Wen <wenslife@outlook.com>
2021-07-11 10:51:47 +0800

    refactor: change default font family

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 final lightTheme = ThemeData(
+  fontFamily: 'Ubuntu',
   primarySwatch: Colors.lightGreen,
   primaryColor: Colors.lightGreen.shade600,
   accentColor: Colors.orangeAccent.shade400,
